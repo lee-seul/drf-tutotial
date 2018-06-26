084cc5f7f94e27958151b55ab0c48c08b3435b8c
seul lee <blacksangi14@naver.com>
2018-06-26 17:34:23 +0900

    using generic CBV

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,40 +1,18 @@
 # coding: utf-8
 
 
-from rest_framework import mixins
 from rest_framework import generics
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer 
 
 
-class SnippetList(generics.GenericAPIView,
-                  mixins.ListModelMixin,
-                  mixins.CreateModelMixin):
+class SnippetList(generics.ListCreateAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(generics.GenericAPIView,
-                    mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
