8785e93e45e9a4d62108039ef3ab03a0170edcee
seul lee <blacksangi14@naver.com>
2018-06-26 17:06:05 +0900

    url suffix

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,6 +2,7 @@
 
 
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 
@@ -9,3 +10,5 @@ urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
