5552ab0047110dd27f216d6f686312b57410e9b9
seul lee <blacksangi14@naver.com>
2018-06-28 14:47:41 +0900

    tutorial part 6

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,44 +1,16 @@
 # coding: utf-8
 
 
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework import renderers
-from snippets import api_root, SnippetViewSet, UserViewSets
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from snippets.views import SnippetViewSet, UserViewSets
 
 
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create',
-})
-
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight',     
-}, renderer_classes=[renderers.StaticHTMLRederer])
-
-user_list = UserViewSets.as_view({
-    'get': 'list'        
-})
-
-user_detail = UserViewSets.as_view({
-    'get': 'retrieve'    
-})
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSets)
 
 
 urlpatterns = [
-    url(r'^$', views.api_root), 
-    url(r'^snippets/$', snippet_list, name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
-    url(r'^users/$', user_list, name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
+    url(r'^', include(router.urls))        
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
